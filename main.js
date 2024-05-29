74a980bc15b0f97e68e97352f8855a8c4245adc6
Omri Nadav <omrinadav15@gmail.com>
2024-05-29 16:08:31 +0300

    Add feats to onBallClick

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 'use strict'
 
-function onBallClick(ev){
-console.log('hi');
+function onBallClick() {
+    var ball = document.querySelector(".ball")
+    // console.log(ball);
+    var computedStyle = getComputedStyle(ball)
+    var currentWidth = parseInt(computedStyle.width)
+    var currentHeight = parseInt(computedStyle.height)
+    // console.log('currentWidth:', currentWidth);
+    // console.log('currentHeight:', currentHeight);
+
+    var newWidth = currentWidth + 50
+    var newHeight = currentHeight + 50
+
+    ball.style.width = newWidth + 'px'
+    ball.style.height = newHeight + 'px'
+
+    var diameter = Math.max(newHeight, newWidth)
+    ball.innerText = diameter 
 }
