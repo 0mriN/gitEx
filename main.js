ce8b02ad38a40d5dc915510344525e2d56ad3ade
Omri Nadav <omrinadav15@gmail.com>
2024-05-29 16:32:16 +0300

    Add limit

--- main.js
+++ main.js
@@ -2,19 +2,19 @@
 
 function onBallClick() {
     var ball = document.querySelector(".ball")
-    // console.log(ball);
     var computedStyle = getComputedStyle(ball)
     var currentWidth = parseInt(computedStyle.width)
     var currentHeight = parseInt(computedStyle.height)
-    // console.log('currentWidth:', currentWidth);
-    // console.log('currentHeight:', currentHeight);
-
     var newWidth = currentWidth + 50
     var newHeight = currentHeight + 50
-
     ball.style.width = newWidth + 'px'
     ball.style.height = newHeight + 'px'
-
     var diameter = Math.max(newHeight, newWidth)
-    ball.innerText = diameter 
+    ball.innerText = diameter + 'px'
+
+    if (diameter >= 400) {
+        ball.style.width = '100px'
+        ball.style.height = '100px'
+        ball.innerText = '100px'
+    }
 }
